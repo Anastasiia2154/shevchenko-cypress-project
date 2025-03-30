11076cf1939fcfa1ebb8c3d2dbbaa8382ef337d6
anastasiiamacbook <anastasia.shevchenko2154@gmail.com>
2025-03-30 16:48:55 +0300

    implemplemented cypress queries homework

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
   screenshotOnRunFailure: true,
   pageLoadTimeout: 60000,
   e2e: {
-    specPattern: "cypress/e2e/**/*.{test,spec}.{js,jsx,ts,tsx}",
+    specPattern: "cypress/**/**/*.{test,spec}.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       on("before:run", (details) => {
         console.log("Tests are about to start!", details);
